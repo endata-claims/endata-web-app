01f86c0fd779b73a10acec850c6ae2512ac12204
dohaicuong <beatyshot@gmail.com>
2020-05-14 15:22:15 +1000

    update redirect on not found route

--- src/pages/NotFound/index.tsx
+++ src/pages/NotFound/index.tsx
@@ -1,6 +1,15 @@
 import React from 'react'
+import { useHistory } from 'react-router-dom'
 
 const NotFound = () => {
+  const token = localStorage.getItem('ACCESS_TOKEN')
+
+  const history = useHistory()
+  React.useEffect(() => {
+    if(!token) history.push('/user/login')
+    else history.push('/claims')
+  }, [token])
+
   return (
     <>Not found 404</>
   )
